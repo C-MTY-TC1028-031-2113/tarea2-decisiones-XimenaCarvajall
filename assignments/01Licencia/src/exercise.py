b289990192f94476be586870391bb190050b2df6
XimenaCarvajall <A01552383@itesm.mx>
2021-08-30 22:22:25 -0500

    300821

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,7 +1,16 @@
 
 def main():
-    pass
+    edad = int(input("Ingresa tu edad: "))
+    if edad>= 18:
+       identificacion = input("¿Tienes identificación? ")
+       if identificacion == "si":
+          print("Trámite de licencia concedido")
+       elif identificacion == "no":
+           print("No cumples con los requisitos")
+       else:
+           print("Respuesta incorrecta")
 
-
+    else:
+        print("No cumples con los requisitos")
 if __name__ == '__main__':
     main()
