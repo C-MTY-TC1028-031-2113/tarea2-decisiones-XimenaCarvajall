d1593561c832cfd6a9896f240ea96c3943165924
XimenaCarvajall <A01552383@itesm.mx>
2021-08-30 22:31:29 -0500

    300821

--- assignments/03BMI/src/exercise.py
+++ assignments/03BMI/src/exercise.py
@@ -1,6 +1,20 @@
 def main():
-    #escribe tu código abajo de esta línea
-    pass
-
+    peso = float(input("Dame tu peso en kilogramos: "))
+    altura = float(input("Dame tu altura en metros: "))
+    if altura <=0 or peso <=0:
+        print("Revisa tus datos, alguno de ellos es incorrecto")
+    else:
+        indicemasa = peso / altura**2
+        if indicemasa < 20:
+            print(str("PESO BAJO"))
+        elif 20 <= indicemasa < 25:
+            print(str("PESO NORMAL"))
+        elif 25 <= indicemasa < 30:
+            print(str("SOBREPESO"))
+        elif 30 <= indicemasa < 40:
+            print(str("OBESIDAD"))
+        else:
+            print(str("OBESIDAD MORBIDA"))
+            
 if __name__=='__main__':
     main()
