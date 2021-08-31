06ba99475641b8ec0e9cb02c6b73723089285fd2
XimenaCarvajall <A01552383@itesm.mx>
2021-08-30 22:44:47 -0500

    300821

--- assignments/05Cuadratica/src/exercise.py
+++ assignments/05Cuadratica/src/exercise.py
@@ -1,11 +1,26 @@
 import math
 
 def main():
-    # Escribe tu código abajo de esta línea
+    
     a=int(input("Da el valor de a: "))
     b=int(input("Da el valor de b: "))
     c=int(input("Da el valor de c: "))
-    pass
+    disc = b**2 - 4*a*c
+    if a == 0 and b == 0:
+        print("No tiene solución")
+    elif a == 0 and b != 0:
+        x = -c/b
+        print(x)
+    elif a != 0 and b != 0 and disc > 0:
+        x1 = (-b + math.sqrt(b**2 -4*a*c)) / 2*a
+        x2 = (-b - math.sqrt(b**2 -4*a*c)) / 2*a
+        print(x1)
+        print(x2)
+    elif a != 0 and b != 0 and disc < 0:
+        print("Raíces complejas")
+    elif a != 0 and b != 0 and disc == 0:
+        d = -b/(2*a)
+        print(d)
 
 if __name__ == '__main__':
     main()
